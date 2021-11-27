3ae6cf56713414543ef645a5baf90ae46ff8014d
Icoder9699 <nick_nack_aziko@mail.ru>
2021-11-27 16:35:03 +0500

    1-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card, { CardVariant } from './components/Card';
 
 function App() {
+  const arr = new Array(5).fill("_")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <div className="container">
+        <h1>Hello React!</h1>
+        <div className="flex">
+          {
+            arr.map(i =>  {
+              return (
+                <Card 
+                  onClick={(num) =>  console.log('clicked', num)}
+                  width="150px"
+                  height="200px"
+                  variant={CardVariant.outline}
+                >
+                  {i}
+                </Card>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
